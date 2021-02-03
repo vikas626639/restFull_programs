23859653a00a40af495c30f6f70a1f60c9aaf975
vikas yadav <vikas626639@gmail.com>
2021-02-03 05:46:51 +0530

    java to xml convertion using jax-b api

--- 08-java-jax-b-xml/src/main/java/com/nit/objet_to_xml/Student.java
+++ 08-java-jax-b-xml/src/main/java/com/nit/objet_to_xml/Student.java
@@ -1,14 +1,22 @@
 package com.nit.objet_to_xml;
 
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import lombok.Data;
 @Data
 @XmlRootElement(name = "Student")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Student {
+	@XmlElement( name = "Student-id")
 	private Integer Studentid;
+	@XmlElement(name = "Student-Name")
 	private String studentName;
+	@XmlElement(name = "Student-Skill")
 	private String studentSkill;
+	@XmlElement(name = "Student-Age")
 	private Integer studentAge;
 }
